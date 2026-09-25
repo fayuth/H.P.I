45b8b2953a566fcf7cf4853fd7ce91cd97e909b4
fayuth <fayuthr4@gmail.com>
2026-09-25 01:02:05 -0500

    easy menu added

--- src/main/java/org/hpi/Menus.java
+++ src/main/java/org/hpi/Menus.java
@@ -1,13 +1,49 @@
 package org.hpi;
 
 import org.hpi.act1.Act1Main;
+import org.hpi.act2.Act2Main;
+
+import java.util.Scanner;
 
 public class Menus {
 
     public static void main(String[] args) {
-
-        Act1Main act1main = new Act1Main();
-        act1main.menu1();
+        main();
     }
 
+    public static void main() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println();
+            System.out.println(" H.P.I ");
+            System.out.println("1. Sistema Académico (Taller 1)");
+            System.out.println("2. Sistema de Ventas y Comisiones (Taller 2)");
+            System.out.println("0. Salir");
+            System.out.println(" ");
+            System.out.print("Opción: ");
+
+            int opcion = leerEntero(scanner);
+
+            switch (opcion) {
+                case 1 -> new Act1Main().menu1();
+                case 2 -> new Act2Main().menu2(scanner);
+                case 0 -> {
+                    System.out.println("¡bye iauphogtfeigae!");
+                    return;
+                }
+                default -> System.out.println("Opción inválida.");
+            }
+        }
+    }
+
+    private static int leerEntero(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Opción inválida. Ingresa un número.");
+            scanner.next();
+            System.out.print("Opción: ");
+        }
+        return scanner.nextInt();
+    }
 }
